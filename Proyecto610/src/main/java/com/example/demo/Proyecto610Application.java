59573defcb6b83dad821daae4500dd24bbc6f1c2
gasparkappou <gasparkappou@gmail.com>
2024-09-23 19:26:50 -0300

    subo espacio de proyecto

---
+++ Proyecto610/src/main/java/com/example/demo/Proyecto610Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto610Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto610Application.class, args);
+	}
+
+}
