265e4a7f046d46ffee4a415b79814d4c9a9224c2
gasparkappou <gasparkappou@gmail.com>
2024-09-30 21:00:14 -0300

    cambio de controller a RestController

--- Pokedle/src/main/java/com/example/demo/showPage.java
+++ Pokedle/src/main/java/com/example/demo/showPage.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class showPage {
-	@RequestMapping("/")
-	public String getHomepage() {
+	@RequestMapping("/{id}")
+	public String getHomepage(@PathVariable String id) {
 		//aca iria algun selector que responda con el archivo correcto dentro de la carpeta src/resources/static
-		String pagina = "index";
-		return pagina + ".html";
+		//String pagina = id.length() == 0? "index" : id;
+		//return pagina + ".html";
+		return "Lo que ingresaste es: " + id;
 	}
 }
